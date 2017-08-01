fix overflow error and loss function / add runner.test() method - now available

main.py
@@ -1,23 +1,34 @@
-from config import config
-from shintb import graph_drawer ,svt_data_loader, default_box_control, runner
-
 import tensorflow as tf
+
+from config import config
+from shintb import graph_drawer, svt_data_loader, default_box_control, runner, output_drawer
 
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 
 graphdrawer = graph_drawer.GraphDrawer(config)
 
-dataloader = svt_data_loader.SVTDataLoader('./svt1/train.xml', './svt1/test.xml')
+dataloader = svt_data_loader.SVTDataLoader(config["train_data_xml"], config['test_data_xml'])
 
 dbcontrol = default_box_control.DefaultBoxControl(config, graphdrawer)
 
-runner = runner.Runner(config, graphdrawer, dataloader, dbcontrol)
+outputdrawer = output_drawer.OutputDrawer(config, dbcontrol)
+
+runner = runner.Runner(config, graphdrawer, dataloader, dbcontrol, outputdrawer)
 
 if __name__ == "__main__":
-	flags.DEFINE_string("mode", "train", "train, image")
+	flags.DEFINE_string("mode", "train", "train,test ,image")
+	flags.DEFINE_string("jobname", None, "job name for saving ckpt file")
+	flags.DEFINE_integer("iter", 100000, "iteration for job")
 
 	if FLAGS.mode == "train":
-		runner.train()
+		if FLAGS.jobname ==None :
+			raise FileNotFoundError("jobname 을 입력하지 않았습니다")
+		else :
+			runner.train(FLAGS.jobname, FLAGS.iter)
+
+	elif FLAGS.mode == "test":
+		runner.test(FLAGS.iter)
+
 	elif FLAGS.mode == "image":
 		runner.image()

shintb/default_box_control.py
@@ -1,5 +1,6 @@
 import shintb.utils.box_calculation as boxcal
 import numpy as np
+import tensorflow as tf
 
 
 class DefaultBoxControl:
@@ -67,7 +68,10 @@ class DefaultBoxControl:
 
         return boxes
 
+
+    # pred_conf : [?, 23280, 2]
     def calculate_pos_neg_trueloc(self, pred_conf, gtboxes):
+        print("calculate_pos_neg_trueloc START")
         c = self.config
 
         pos_neg_trueloc = [None for i in range(c["batch_size"])]
@@ -81,14 +85,12 @@ class DefaultBoxControl:
 
         positive, negative, _true_conf ,true_loc = [np.stack(m) for m in zip(*pos_neg_trueloc)]
 
-
+        print("calculate_pos_neg_trueloc END")
         return positive, negative, true_loc
 
-
-
-
+    # pred_conf_i : [23280, 2]
     def matching_dbboxes_gtboxes_in_batch_i(self, pred_conf_i, gtboxes_i):
-
+        print("matching_dbboxes_gtboxes_in_batch_i START")
         c = self.config
 
         #default box들의 좌표와 번째 수를 나열해놓은 리스트
@@ -204,6 +206,7 @@ class DefaultBoxControl:
         negative_max = positive_count * c["neg/pos"]
         negative_count = 0
 
+        # pred_conf_i : [ 23280, 2]
         top_confidences = self.get_top_confidences(pred_conf_i, negative_max)
         # top confidence 들의 index list 를 반환
 
@@ -229,22 +232,48 @@ class DefaultBoxControl:
 
         #print("%i positives" % positive_count)
         #print("%i/%i negatives" % (negative_count, negative_max))
-
+        print("matching_dbboxes_gtboxes_in_batch_i END")
         return matches
 
-    def get_top_confidences(self, pred_conf, negative_max):
+    #pred_conf_i : [23280, 2]
+    def get_top_confidences(self, pred_conf_i, negative_max):
+        print("get_top_confidences START")
         confidences = []
 
+        # pred_conf_i : [23280, 2]
+        #logits : [c1, c2]
+
+        # 오버플로우 방지!
+        def softmax_without_overflow(a, reduce_axis=1) :
+            c = np.max(a, reduce_axis)
+            c = np.reshape(c, [-1,1])
+            exp_a = np.exp(a - c)
+            sum_exp_a = a.sum(reduce_axis)
+            sum_exp_a = np.reshape(sum_exp_a, [-1, 1])
+
+            return exp_a / (sum_exp_a + 1.0e-4)
+
+        pred_conf_i_softmax =  softmax_without_overflow(pred_conf_i)
+        # pred_conf_i_softmax = tf.nn.softmax(pred_conf_i, -1)
+
+        for probs in pred_conf_i_softmax :
+            top_confidence = np.amax(probs)
+            confidences.append(top_confidence)
+
+        '''
         for logits in pred_conf:
-            probs = np.exp(logits) / np.add(np.sum(np.exp(logits)), 1e-3)
+            #probs = (1.0/np.exp(-logits)) / np.add(np.sum((1.0/np.exp(-logits))), 1e-3)
+            probs = tf.nn.softmax(logits)
+            # probs : [softmax_c1, softmax_c2]
             top_label = np.amax(probs)
             confidences.append(top_label)
-
+        '''
         # top_confidences = sorted(confidences, key=lambda tup: tup[1])[::-1]
 
         k = min(negative_max, len(confidences))
         top_confidences = np.argpartition(np.asarray(confidences), -k)[-k:]
 
+        print("get_top_confidences END")
         return top_confidences
 
     # prepare_feed  (이미지 하나에 대하여)
@@ -255,7 +284,7 @@ class DefaultBoxControl:
     # a_true_labels (23280, )
     # a_true_locs (23280, 4)
     def prepare_pos_neg_trueloc_in_matches(self, matches):
-
+        print("prepare_pos_neg_trueloc_in_matches START")
         c = self.config
 
         positives_list = []
@@ -298,6 +327,7 @@ class DefaultBoxControl:
         a_true_labels = np.asarray(true_labels_list)
         a_true_locs = np.asarray(true_locs_list)
 
+        print("prepare_pos_neg_trueloc_in_matches END  ")
         return a_positives, a_negatives, a_true_labels, a_true_locs
 
 

shintb/graph_drawer.py
@@ -16,6 +16,7 @@ class GraphDrawer:
         self.draw_loss_function()
         self.draw_optimizer()
         self.init_op = self.draw_init_op()
+        self.draw_summary_op()
         print("Complete !!!")
 
     def draw_placeholders(self):
@@ -107,23 +108,30 @@ class GraphDrawer:
 
         with tf.name_scope(name="predictions") as scope :
             self.pred_conf , self.pred_loc = tf.split(pred_boxes, [2,4], axis = 2)
-            #self.pred_confs : [?, 23280 , 2]
-            #self.pred_locs : [? ,23280, 4]
+            #self.pred_conf : [?, 23280 , 2]
+            #self.pred_loc : [? ,23280, 4]
 
         print("Done!")
 
     def draw_loss_function(self):
         print(">>>Draw loss function ...", end=" ")
-        text_conf , background_conf = tf.split(self.pred_conf, 2, 2)
+        # [?, 23280, 2]
+        pred_conf_softmax =  tf.nn.softmax(self.pred_conf, -1)
+        # [?, 23280, 1], [?, 23280, 1]
+        text_conf , background_conf = tf.split(pred_conf_softmax, 2, 2)
+        # [?, 23280]
         text_conf = tf.reshape(text_conf, [-1, text_conf.get_shape().as_list()[1]] )
+        # [?, 23280]
         background_conf = tf.reshape(background_conf, [-1, background_conf.get_shape().as_list()[1]])
-        self.conf_loss =  - tf.reduce_sum(tf.log(tf.nn.softmax(text_conf))*self.positive_ph) - tf.reduce_sum(tf.log(tf.nn.softmax(background_conf))*self.negative_ph)
+        self.conf_loss =  - tf.reduce_sum(tf.log(text_conf)*self.positive_ph) - tf.reduce_sum(tf.log(background_conf)*self.negative_ph)
 
         loc_loss = tf.reduce_sum(gc.smooth_l1(self.pred_loc - self.true_loc_ph), reduction_indices= 2) * self.positive_ph
         self.loc_loss = tf.reduce_sum(loc_loss, reduction_indices= 1) / (tf.reduce_sum(self.positive_ph, reduction_indices = 1) + 1e-5)
         # 1e-5 는 분모가 0일 경우를 방지
 
         self.total_loss = tf.reduce_mean(self.conf_loss + self.loc_loss)
+
+
         print("Done!")
 
     def draw_optimizer(self):
@@ -137,3 +145,13 @@ class GraphDrawer:
     def draw_init_op(self):
         init_op = tf.global_variables_initializer()
         return init_op
+
+    def draw_summary_op(self):
+        print(">>>Draw summary op ...", end=" ")
+        tf.summary.scalar("loss/conf_loss", tf.reshape(tf.reduce_sum(self.conf_loss), shape=[]))
+        tf.summary.scalar("loss/loc_loss", tf.reshape(tf.reduce_sum(self.loc_loss), shape=[]))
+        tf.summary.scalar("loss/total_loss",  tf.reshape(self.total_loss, shape=[]))
+
+        self.summaries = tf.summary.merge_all()
+        print("Done!")
+

shintb/runner.py
@@ -1,44 +1,69 @@
 import tensorflow as tf
+import cv2
+
 import time
 from shintb.utils.learning_rate_maker import learning_rate_maker
 
+
 class Runner :
-    def __init__(self, config, graphdrawer, dataloader, dbcontrol):
+    def __init__(self, config, graphdrawer, dataloader, dbcontrol, outputdrawer):
         print("Runner instance initialing ...")
         print(">>> Collecting config info, GraphDrawer instance, DataLoader instance , DefaultBoxControl instance... Done!")
-        self.sess = tf.Session(graph = tf.get_default_graph())
-        self.saver = tf.train.Saver()
+        #self.sess = tf.Session(graph = tf.get_default_graph(), config=tf.ConfigProto(log_device_placement=True))
+        self.sess = tf.Session(graph=tf.get_default_graph())
+        self.saver = tf.train.Saver()  # tensorflow ckpt poto buff
+          # tensorflow summary proto buff
 
         self.config = config
         self.graph = graphdrawer
         self.dataloader = dataloader
         self.dbcontrol = dbcontrol
+        self.outputdrawer = outputdrawer
         print("Complete !!!")
 
-    def train(self):
-        print("Runner starts training ...")
+    def train(self, jobname, iter):
+        print("Runner starts training job...")
+        self.writer = tf.summary.FileWriter(logdir=(self.config["saved_dir"] + "/" + jobname))
+
 
         c = self.config
         g = self.graph
-        print(tf.global_variables()[0])
+        #print(tf.global_variables()[0])
         self.sess.run(tf.global_variables_initializer())
 
-        ckpt = tf.train.get_checkpoint_state(c["model_dir"])
+        ckpt = tf.train.get_checkpoint_state(c["saved_dir"])
 
         if ckpt and ckpt.model_checkpoint_path:
+            last_step = int(ckpt.model_checkpoint_path.split("-")[-1])
             self.saver.restore(self.sess, ckpt.model_checkpoint_path)
-            print("!! RESTORE SAVED VARIBALES IN 'model_dir' !! : restored %s" % ckpt.model_checkpoint_path)
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("!! RESTORE SAVED VARIBALES !! : restored %s" % ckpt.model_checkpoint_path)
+            print("!! RESTORE SAVED VARIBALES !! : restart from step " , last_step)
+            print("!! RESTORE SAVED VARIBALES !! : restored %s" % ckpt.model_checkpoint_path)
+            print("!! RESTORE SAVED VARIBALES !! : restored %s" % ckpt.model_checkpoint_path)
+            print("!! RESTORE SAVED VARIBALES !! : restored %s" % ckpt.model_checkpoint_path)
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 
-        while True:
+        step = 0
+        while step < iter :
 
             start_t = time.time()
 
-            print(">>>", c["batch_size"] ," random batch data selected by DataLoader...", end=" ")
+            print(">>> 8 random batch data selected by DataLoader...", end=" ")
             train_imgs, train_gtboxes = self.dataloader.nextBatch(c["batch_size"])
             print("Done!")
+
+            print(">>> from graph, get pred_conf, pred_loc ...", end =" ")
             pred_conf, pred_loc, global_step = self.sess.run([g.pred_conf, g.pred_loc, g.global_step],
                                                              feed_dict = {g.imgs_ph : train_imgs,
                                                                           g.batch_norm_ph : False})
+            print("Done!")
 
             print(">>>calculating (positive, negative, true_location) with jaccard overlap by DefaultBoxControl ...", end=" ")
             positive, negative , true_loc = self.dbcontrol.calculate_pos_neg_trueloc(pred_conf, train_gtboxes)
@@ -71,7 +96,180 @@ class Runner :
             print("GLOBAL STEP :",global_step, "/ LEARNING RATE :", learning_rate, "/ LOSS :", total_loss, " (",during_t, "secs)" )
 
             if global_step % 1000 == 0 :
-                self.saver.save(self.sess, "%s.ckpt" % c["model_dir"],global_step)
+                ckpt_path = self.saver.save(self.sess, "%s.ckpt" % (c["saved_dir"]+"/"+jobname),global_step)
+                print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+                print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+                print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+                print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+                print("SAVED AT : ", ckpt_path)
+                print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+                print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+                print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+                print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+            summ_proto_buff=self.sess.run(g.summaries, feed_dict={g.imgs_ph: train_imgs,
+                                                                                    g.true_loc_ph: true_loc,
+                                                                                    g.positive_ph: positive,
+                                                                                    g.negative_ph: negative,
+                                                                                    g.learning_rate_ph: learning_rate,
+                                                                                    g.batch_norm_ph: True
+                                                                                    })
+            self.writer.add_summary(summ_proto_buff, global_step = global_step)
+            step += 1
+
+        ckpt_path = self.saver.save(self.sess, "%s.ckpt" % (c["saved_dir"]+"/"+jobname), global_step)
+
+        print("::: TRAINING JOB FINISHED ::: \n::: CKPT SAVED AT", ckpt_path," ::: \n:::  SUMMARY LOG SAVED AT ", (c["saved_dir"]+"/"+jobname) )
+        print("TIP : LAUNCH tensorboard WITH ...  >> tensorboard --logdir=",(c["saved_dir"]+"/"+jobname))
+
+
+    def test(self, iter):
+
+        print("Runner starts finding texts on test dataset ...")
+        c = self.config
+        g = self.graph
+        self.sess.run(tf.global_variables_initializer())
+
+        ckpt = tf.train.get_checkpoint_state(c["saved_dir"])
+
+        if ckpt and ckpt.model_checkpoint_path:
+            self.saver.restore(self.sess, ckpt.model_checkpoint_path)
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("!! RESTORE SAVED VARIBALES !! : restored %s" % ckpt.model_checkpoint_path)
+            print("!! RESTORE SAVED VARIBALES !! : restored %s" % ckpt.model_checkpoint_path)
+            print("!! RESTORE SAVED VARIBALES !! : restored %s" % ckpt.model_checkpoint_path)
+            print("!! RESTORE SAVED VARIBALES !! : restored %s" % ckpt.model_checkpoint_path)
+            print("!! RESTORE SAVED VARIBALES !! : restored %s" % ckpt.model_checkpoint_path)
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+            cv2.namedWindow("outputs", cv2.WINDOW_NORMAL)
+
+            step = 0
+            while step < iter:
+
+                print(">>>  3 random batch data selected by DataLoader...", end=" ")
+                test_imgs, test_gtboxes = self.dataloader.nextBatch(3, 'test')
+                print("Done!")
+
+                print("IMGS INPUT: ", test_imgs[0], test_imgs.shape)
+                print("GROUND TRUTH BOX INFO :", test_gtboxes[0])
+
+                pred_conf, pred_loc, global_step =  self.sess.run([g.pred_conf, g.pred_loc, g.global_step],
+                                                                    feed_dict={g.imgs_ph: test_imgs,
+                                                                    g.batch_norm_ph: False})
+
+                print("GRAPH INPUT:", self.sess.run(g.input, feed_dict={g.imgs_ph: test_imgs}))
+
+                out1, out2, out3, out4, out5, out6 = self.sess.run([g.out1,g.out2,g.out3,g.out4,g.out5,g.out6],
+                                                                    feed_dict={g.imgs_ph: test_imgs,
+                                                                    g.batch_norm_ph: False})
+
+                conv1_1, conv1_2, conv4_3, conv7 = self.sess.run([g.conv1_1,g.conv1_2,g.conv4_3, g.conv7],
+                                                                    feed_dict={g.imgs_ph: test_imgs,
+                                                                    g.batch_norm_ph: False})
+
+                print("CONV1_1 INFO : ", conv1_1)
+                print("CONV1_2 INFO : ", conv1_2)
+                print("CONV4_3 INFO : ", conv4_3)
+                print("CONV7 INFO : ", conv7)
+
+
+                print("MAP POINT 1 INFO : ", out1)
+                #print("MAP POINT 2 INFO : ", out2)
+                #print("MAP POINT 3 INFO : ", out3)
+                #print("MAP POINT 4 INFO : ", out4)
+                #print("MAP POINT 5 INFO : ", out5)
+                #print("MAP POINT 6 INFO : ", out6)
+
+
+                print("PREDICTED CONFIDENCE : ", pred_conf[0], pred_conf.shape)
+                print("PREDICTED LOCATION :", pred_loc[0], pred_loc.shape)
+
+                output_boxes, output_confidence = self.outputdrawer.format_output(pred_conf[0], pred_loc[0])
+
+                self.outputdrawer.draw_outputs(test_imgs[0], output_boxes, output_confidence, wait=1)
+
+                step += 1
+
+
+
+
+
+
+
+
+        else :
+            raise FileNotFoundError("ckpt저장 폴더에서 불러올 ckpt 파일을 찾지 못했습니다")
+
+
+
+
 
     def image(self):
-        return 0
+        print("Runner starts finding texts on test dataset ...")
+        c = self.config
+        g = self.graph
+        self.sess.run(tf.global_variables_initializer())
+
+        ckpt = tf.train.get_checkpoint_state(c["saved_dir"])
+
+        if ckpt and ckpt.model_checkpoint_path:
+            self.saver.restore(self.sess, ckpt.model_checkpoint_path)
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("!! RESTORE SAVED VARIBALES !! : restored %s" % ckpt.model_checkpoint_path)
+            print("!! RESTORE SAVED VARIBALES !! : restored %s" % ckpt.model_checkpoint_path)
+            print("!! RESTORE SAVED VARIBALES !! : restored %s" % ckpt.model_checkpoint_path)
+            print("!! RESTORE SAVED VARIBALES !! : restored %s" % ckpt.model_checkpoint_path)
+            print("!! RESTORE SAVED VARIBALES !! : restored %s" % ckpt.model_checkpoint_path)
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+            print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+            #cv2.namedWindow("outputs", cv2.WINDOW_NORMAL)
+
+
+
+
+            print(">>> 1 random batch data selected by DataLoader...", end=" ")
+            test_img, test_gtbox = self.dataloader.nextBatch(1, 'test')
+            print("Done!")
+
+            pred_conf, pred_loc, global_step =  self.sess.run([g.pred_conf, g.pred_loc, g.global_step],
+                                                                    feed_dict={g.imgs_ph: test_img,
+                                                                    g.batch_norm_ph: False})
+
+
+
+
+            output_boxes, output_confidence = self.outputdrawer.format_output(pred_conf[0,:,:], pred_loc[0,:,:])
+
+            #nmsed_list = self.outputdrawer.postprocess_boxes(output_boxes, output_confidence, 0.01, 0.45)
+
+
+
+            self.outputdrawer.draw_outputs(test_img, output_boxes , output_confidence , wait=1)
+
+
+
+
+
+
+
+
+
+
+        else :
+            raise FileNotFoundError("ckpt저장 폴더에서 불러올 ckpt 파일을 찾지 못했습니다")
+
+
+

shintb/utils/graph_components.py
@@ -2,12 +2,15 @@ import tensorflow as tf
 
 
 def weightVariable(shape):
-	initial = tf.truncated_normal(shape, stddev=0.1, name='weights')
+	initial = tf.truncated_normal(shape ,stddev=0.1, name='weights')
 	return tf.Variable(initial)
+
+	#return tf.get_variable(name="W", shape=shape, initializer=tf.random_normal_initializer(mean=0.0, stddev = 0.1))
 
 def biasVariable(shape):
 	initial = tf.constant(0.1, shape=shape, name='biases')
 	return tf.Variable(initial)
+	#return tf.get_variable(name="B", shape=shape, initializer=tf.random_normal_initializer(mean=0.0, stddev=0.1))
 
 def maxPool(input, stride=2, kernel=2, padding='SAME', name='pool'):
 	return tf.nn.max_pool(input, ksize=[1, kernel, kernel, 1], strides=[1, stride, stride, 1], padding='SAME', name=name)